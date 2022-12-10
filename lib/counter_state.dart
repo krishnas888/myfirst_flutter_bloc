87f9e25fc817733278e5aa632af7e8bd218946cc
krishnas888 <verakrishnas@gmail.com>
2022-12-10 09:25:53 +0530

    block listener and bloc builder implement

--- lib/counter_state.dart
+++ lib/counter_state.dart
@@ -2,8 +2,9 @@ part of 'counter_cubit.dart';
 
 class CounterState {
   int counterValue;
+  bool? wasIncrement;
   CounterState({
-    required this.counterValue,
+    required this.counterValue, this.wasIncrement,
 });
 }
 
